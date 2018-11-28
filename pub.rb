27fc05cc24ff4eb7c45877d75a5f90f788ff3a23
Michael Irwin <mike.irwin1@gmail.com>
2018-11-28 11:40:02 +0000

    customer purchase increased value of till up

--- pub.rb
+++ pub.rb
@@ -3,10 +3,18 @@ class Pub
   attr_reader :name, :drinks
   attr_accessor :till
 
-  def initialize(name, till, drinks)
+  def initialize(name, till)
     @name = name
     @till = till
-    @drinks = drinks
+    @drinks = []
+  end
+
+  def add_drinks(drinks)
+    @drinks.concat(drinks)
+  end
+
+  def customer_buy(drink)
+    @till += drink.price
   end
 
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -2,15 +2,24 @@ require('minitest/autorun')
 require('minitest/rg')
 
 require_relative('../pub')
+require_relative('../customers')
+require_relative('../drinks')
 
 class PubTest < MiniTest::Test
 
   def setup
-    @pub = Pub.new("The Arms", 0, [])
+    @pub = Pub.new("The Bear Arms", 0)
+    @drink1 = Drinks.new("whisky", 3)
+    @drink2 = Drinks.new("wine", 2)
+    @drink3 = Drinks.new("irish car bomb", 5)
+    @customer = Customer.new("Billy", 20, 30)
+    @drinks = [@drink1, @drink2, @drink3]
+
+    @pub.add_drinks(@drinks)
   end
 
   def test_pub_has_name
-    assert_equal("The Arms", @pub.name)
+    assert_equal("The Bear Arms", @pub.name)
   end
 
   def test_pub_has_money
@@ -18,7 +27,11 @@ class PubTest < MiniTest::Test
   end
 
   def test_pub_has_drinks
-    assert_equal([], @pub.drinks)
+    assert_equal(@drinks, @pub.drinks)
+  end
+
+  def test_customer_buys_drink_till_value_goes_up
+    assert_equal(3,@pub.customer_buy(@drink1))
   end
 
 end
